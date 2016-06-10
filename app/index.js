b957270be93951a2bf0af531cf61df03bbbf76d3
Niklas Lagergren <lani@lani.nu>
2016-06-10 21:45:23 +0200

    trying out polythene

--- app/index.js
+++ app/index.js
@@ -2,20 +2,27 @@ import m from 'mithril'
 import routes from 'routes'
 
 /* eslint-disable no-unused-vars */
+import 'polythene/theme/theme'
 import styles from 'styles'
 /* eslint-enable no-unused-vars */
+
+import button from 'polythene/button/button'
 
 m.route.mode = 'hash'
 
 const header = {
   controller: function () {},
   view: function () {
-    return m('div',
-      routes.map((route) => m('a', {
-        href: route.path,
-        config: m.route,
+    return m('div', [
+      routes.map((route) => m(button, {
+        label: route.title,
+        url: {
+          href: route.path,
+          config: m.route
+        },
         class: m.route() === route.path ? 'active' : ''
-      }, route.title))
+      })),
+      m(button, { label: 'Test', raised: true }) ]
     )
   }
 }
